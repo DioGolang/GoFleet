a1e1bb56437a6b33e9da706ac05fed5008ac4572
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-15 02:37:08 -0300

    :sparkles: feat: create dto order

---
+++ internal/application/dto/create_order.go
@@ -0,0 +1,16 @@
+package dto
+
+// Input
+
+type CreateOrderInput struct {
+	ID    string  `json:"id"`
+	Price float64 `json:"price"`
+	Tax   float64 `json:"tax"`
+}
+
+// Output
+
+type CreateOrderOutput struct {
+	ID         string  `json:"id"`
+	FinalPrice float64 `json:"final-price"`
+}
