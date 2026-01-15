7d37433d242b214e82c0002b1b07e3b18002c9a5
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-15 14:53:47 -0300

    :sparkles: feat: interfaces Event

---
+++ pkg/events/interfaces.go
@@ -0,0 +1,22 @@
+package events
+
+import "time"
+
+type Event interface {
+	GetName() string
+	GetDateTime() time.Time
+	GetPayload() interface{}
+	SetPayload(payload interface{})
+}
+
+type EventDispatcher interface {
+	Register(eventName string, handler EventHandler) error
+	Dispatch(event Event) error
+	Remove(eventName string, handler EventHandler) error
+	Has(eventName string, handler EventHandler) bool
+	Clear()
+}
+
+type EventHandler interface {
+	Handler(event Event)
+}
