5269d40de70f3dc79bfcced97ad503da0eadd8a1
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-15 14:41:25 -0300

    :sparkles: feat: event dispatcher

---
+++ internal/infra/event/event_dispatcher.go
@@ -0,0 +1,42 @@
+package event
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/DioGolang/GoFleet/pkg/events"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"time"
+)
+
+type Dispatcher struct {
+	RabbitMQChannel *amqp.Channel
+}
+
+func NewDispatcher(ch *amqp.Channel) *Dispatcher {
+	return &Dispatcher{RabbitMQChannel: ch}
+}
+
+func (ed *Dispatcher) Dispatch(event events.Event) error {
+	payload, err := json.Marshal(event.GetPayload())
+	if err != nil {
+		return err
+	}
+
+	err = ed.RabbitMQChannel.PublishWithContext(
+		context.Background(),
+		"amq.direct",
+		"orders.created",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Timestamp:   time.Now(),
+			Body:        payload,
+		})
+	return err
+}
+
+func (ed *Dispatcher) Register(eventName string, handler events.EventHandler) error { return nil }
+func (ed *Dispatcher) Remove(eventName string, handler events.EventHandler) error   { return nil }
+func (ed *Dispatcher) Has(eventName string, handler events.EventHandler) bool       { return false }
+func (ed *Dispatcher) Clear()                                                       {}
