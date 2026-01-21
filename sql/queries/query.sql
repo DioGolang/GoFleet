b05f9665b6e3588c3979ba084a77ee32c65f3a83
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-20 22:07:23 -0300

    :sparkles: feat: update query with new fields

--- sql/queries/query.sql
+++ sql/queries/query.sql
@@ -1,10 +1,16 @@
 -- name: CreateOrder :exec
-INSERT INTO orders (id, price, tax, final_price)
-VALUES ($1, $2, $3, $4);
+INSERT INTO orders (id, price, tax, final_price, status, driver_id)
+VALUES ($1, $2, $3, $4, $5, $6);
 
 -- name: GetOrder :one
-SELECT id, price, tax, final_price FROM orders
+SELECT id, price, tax, final_price, status, driver_id FROM orders
 WHERE id = $1;
 
 -- name: ListOrders :many
 SELECT id, price, tax, final_price FROM orders;
+
+-- name: UpdateOrderStatus :exec
+UPDATE orders
+SET status = $1, driver_id = $2
+WHERE id = $3;
+
