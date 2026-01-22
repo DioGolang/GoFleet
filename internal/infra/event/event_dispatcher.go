a377727c1c253ec59d97d79cf9f0500bcb658ebe
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-22 16:43:56 -0300

    :sparkles: feat: accepts ctx as a parameter and otel implementation for traces

--- internal/infra/event/event_dispatcher.go
+++ internal/infra/event/event_dispatcher.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/DioGolang/GoFleet/pkg/events"
+	carrier "github.com/DioGolang/GoFleet/pkg/otel"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.opentelemetry.io/otel"
 	"time"
 )
 
@@ -16,19 +18,23 @@ func NewDispatcher(ch *amqp.Channel) *Dispatcher {
 	return &Dispatcher{RabbitMQChannel: ch}
 }
 
-func (ed *Dispatcher) Dispatch(event events.Event) error {
+func (ed *Dispatcher) Dispatch(ctx context.Context, event events.Event) error {
+	headers := make(amqp.Table)
+	otel.GetTextMapPropagator().Inject(ctx, carrier.AMQPHeadersCarrier(headers))
+
 	payload, err := json.Marshal(event.GetPayload())
 	if err != nil {
 		return err
 	}
 
 	err = ed.RabbitMQChannel.PublishWithContext(
-		context.Background(),
+		ctx,
 		"amq.direct",
 		"orders.created",
 		false,
 		false,
 		amqp.Publishing{
+			Headers:     headers,
 			ContentType: "application/json",
 			Timestamp:   time.Now(),
 			Body:        payload,
