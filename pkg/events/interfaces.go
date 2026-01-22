a8720b4d3ceb8acab294212e36a5ea2ab51a59ac
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-22 16:44:38 -0300

    :sparkles: feat: add ctx with param

--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -1,6 +1,9 @@
 package events
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Event interface {
 	GetName() string
@@ -11,7 +14,7 @@ type Event interface {
 
 type EventDispatcher interface {
 	Register(eventName string, handler EventHandler) error
-	Dispatch(event Event) error
+	Dispatch(ctx context.Context, event Event) error
 	Remove(eventName string, handler EventHandler) error
 	Has(eventName string, handler EventHandler) bool
 	Clear()
