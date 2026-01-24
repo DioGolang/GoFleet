4f458adbae30fa459ba76e8453e76f7cdb334a10
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-24 00:12:16 -0300

    :sparkles: feat:  handler wapper

---
+++ internal/infra/event/metrics_wrapper.go
@@ -0,0 +1,42 @@
+package event
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/DioGolang/GoFleet/pkg/metrics"
+	"github.com/sony/gobreaker"
+)
+
+type MessageHandler func(ctx context.Context, msg []byte) error
+
+func WrapResilientConsumer(
+	m metrics.Metrics,
+	handlerName string,
+	timeout time.Duration,
+	cb *gobreaker.CircuitBreaker,
+	next MessageHandler,
+) MessageHandler {
+	return func(ctx context.Context, msg []byte) error {
+		start := time.Now()
+
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		_, err := cb.Execute(func() (interface{}, error) {
+			return nil, next(ctx, msg)
+		})
+
+		if errors.Is(err, gobreaker.ErrOpenState) {
+			m.RecordUseCaseExecution(handlerName, false, time.Since(start))
+			return err
+		}
+
+		duration := time.Since(start)
+		success := err == nil
+		m.RecordUseCaseExecution(handlerName, success, duration)
+
+		return err
+	}
+}
