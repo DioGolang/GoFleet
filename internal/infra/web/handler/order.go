e397fca6ffd555d6f60cc1a028d09bd9dea26c86
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-26 00:29:06 -0300

    :sparkles: feat: add logs handler

--- internal/infra/web/handler/order.go
+++ internal/infra/web/handler/order.go
@@ -3,21 +3,25 @@ package handler
 import (
 	"encoding/json"
 	"github.com/DioGolang/GoFleet/internal/application/usecase/order"
+	"github.com/DioGolang/GoFleet/pkg/logger"
 	"net/http"
 )
 
 type Order struct {
 	EventService       any
 	CreateOrderUseCase order.CreateUseCase
+	Logger             logger.Logger
 }
 
-func NewOrderHandler(uc order.CreateUseCase) *Order {
+func NewOrderHandler(uc order.CreateUseCase, l logger.Logger) *Order {
 	return &Order{
 		CreateOrderUseCase: uc,
+		Logger:             l,
 	}
 }
 
 func (h *Order) Create(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	var dto order.CreateInput
 
 	err := json.NewDecoder(r.Body).Decode(&dto)
@@ -25,8 +29,17 @@ func (h *Order) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
+	h.Logger.Info(ctx, "creating new order",
+		logger.String("order_id", dto.ID),
+		logger.String("customer_id", dto.ID),
+	)
+
 	output, err := h.CreateOrderUseCase.Execute(r.Context(), dto)
 	if err != nil {
+		h.Logger.Error(ctx, "order creation failed",
+			logger.WithError(err),
+			logger.String("order_id", dto.ID),
+		)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
