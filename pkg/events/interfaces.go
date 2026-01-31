5a5382ef6e5e1baf48843e42e68c3a60a138db88
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-01-31 00:52:14 -0300

    :sparkles: feat: new signature

--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -15,6 +15,7 @@ type Event interface {
 type EventDispatcher interface {
 	Register(eventName string, handler EventHandler) error
 	Dispatch(ctx context.Context, event Event) error
+	DispatchRaw(ctx context.Context, routingKey string, payload []byte) error
 	Remove(eventName string, handler EventHandler) error
 	Has(eventName string, handler EventHandler) bool
 	Clear()
