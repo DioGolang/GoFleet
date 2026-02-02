f7ef181665dac4aad119789dbe60670d2b6c9cb3
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-02-02 01:47:03 -0300

    :bug: fix: interfaces impl

--- internal/infra/database/unit_of_work.go
+++ internal/infra/database/unit_of_work.go
@@ -8,6 +8,18 @@ import (
 	"github.com/DioGolang/GoFleet/internal/application/port/outbound"
 )
 
+type RepositoryProviderImpl struct {
+	db      *sql.DB
+	queries *Queries
+}
+
+func (p *RepositoryProviderImpl) Order() outbound.OrderRepository {
+	return &OrderRepositoryImpl{
+		Db:      p.db,
+		Queries: p.queries,
+	}
+}
+
 type UnitOfWorkImpl struct {
 	db *sql.DB
 }
@@ -16,18 +28,18 @@ func NewUnitOfWork(db *sql.DB) *UnitOfWorkImpl {
 	return &UnitOfWorkImpl{db: db}
 }
 
-func (u *UnitOfWorkImpl) Do(ctx context.Context, fn func(repo outbound.OrderRepository) error) error {
+func (u *UnitOfWorkImpl) Do(ctx context.Context, fn func(provider outbound.RepositoryProvider) error) error {
 	tx, err := u.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 
-	txRepo := &OrderRepositoryImpl{
-		Db:      u.db,
-		Queries: New(u.db).WithTx(tx), // SQLC magic
+	provider := &RepositoryProviderImpl{
+		db:      u.db,
+		queries: New(u.db).WithTx(tx), // SQLC magic
 	}
 
-	if err := fn(txRepo); err != nil {
+	if err := fn(provider); err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
