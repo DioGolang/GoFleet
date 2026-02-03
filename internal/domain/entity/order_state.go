afd26c10fb1ccc6f5dc3d24f1830d8989e9d5720
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-02-03 00:25:28 -0300

    :sparkles: feat: add send to manual method

--- internal/domain/entity/order_state.go
+++ internal/domain/entity/order_state.go
@@ -7,6 +7,7 @@ var ErrInvalidStateTransition = errors.New("invalid state transition")
 type OrderState interface {
 	Name() string
 	Dispatch(o *Order, driverID string) error
+	SendToManual(o *Order) error
 	Deliver(o *Order) error
 	Cancel(o *Order) error
 }
