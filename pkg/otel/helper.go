0d6731bd7e48d9c82dec3aa1ae6978949a7d6a6e
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-02-03 20:22:53 -0300

    :sparkles: feat: helper otel

---
+++ pkg/otel/helper.go
@@ -0,0 +1,34 @@
+package otel
+
+import (
+	"context"
+	"encoding/json"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+func ExtractContextToJSON(ctx context.Context) []byte {
+	carrier := propagation.MapCarrier{}
+
+	otel.GetTextMapPropagator().Inject(ctx, carrier)
+
+	b, err := json.Marshal(carrier)
+	if err != nil {
+		return nil
+	}
+	return b
+}
+
+func InjectContextFromJSON(parentCtx context.Context, data []byte) context.Context {
+	if len(data) == 0 {
+		return parentCtx
+	}
+
+	carrier := propagation.MapCarrier{}
+	if err := json.Unmarshal(data, &carrier); err != nil {
+		return parentCtx
+	}
+
+	return otel.GetTextMapPropagator().Extract(parentCtx, carrier)
+}
