add0154134c977f398063e87c20a0814f32184f0
dvasconcelos <di0g0vasconcelos@hotmail.com>
2026-02-03 04:13:55 -0300

    :recycle: refactor: update contracts

--- internal/application/port/outbound/order_repository.go
+++ internal/application/port/outbound/order_repository.go
@@ -8,7 +8,7 @@ import (
 
 type OrderRepository interface {
 	Save(ctx context.Context, order *entity.Order) error
-	SaveOutboxEvent(ctx context.Context, eventID, aggID, eventType string, payload []byte, topic string) error
+	SaveOutboxEvent(ctx context.Context, eventID, aggID, eventType string, eventVersion int32, payload []byte, topic string) error
 	FindByID(ctx context.Context, id string) (*entity.Order, error)
 	UpdateStatus(ctx context.Context, id string, status string, driverID string) error
 }
